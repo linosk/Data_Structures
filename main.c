47e0f00240dcd4e4cbd882ffa0af3f9daa31cc63
linosk <linakoskar@gmail.com>
2022-11-18 18:33:08 +0100

    Content of main deleted, further work needed - sorting needs to be implemented and trying to standarize whole code.

--- main.c
+++ main.c
@@ -2,17 +2,5 @@
 
 int main(int argc, char** argv)
 {
-    //using double pointers is about copying by value or something like that
-    LLNode* Head = NULL;
-    ll_insert_at_beg(&Head,3);
-    ll_insert_at_beg(&Head,7);
-    ll_insert_at_end(&Head,1);
-    ll_insert_at_end(&Head,2);
-    ll_insert_at_end(&Head,10);
-
-    ll_print_values(Head);
-
-    ll_destroy(Head);
-
     return 0;
 }
