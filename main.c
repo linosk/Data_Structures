ef79207eb894ee1aef6582a0a95878f09dc7049f
linosk <linakoskar@gmail.com>
2023-01-02 18:09:07 +0100

    Tree structure - beginning

--- main.c
+++ main.c
@@ -1,20 +1,19 @@
-#include "graph.h"
+#include "tree.h"
 
 int main(int argc, char** argv)
 {
-    Graph* ne = create_graph(4);
+    Tree_node* Root = NULL;
+    bst_insert(&Root,4);
+    bst_insert(&Root,3);
+    bst_insert(&Root,5);
+    bst_insert(&Root,1);
+    bst_insert(&Root,2);
+    bst_insert(&Root,6);
+    bst_insert(&Root,1);
 
-    declare_graph_structure_from_file(ne);
+    bst_display(Root);
 
-    //declare_graph_structure(ne);
-
-    print_graph_values(ne);
-
-    dfs(ne);
-    fprintf(stdout,"-----------------\n");
-    bfs(ne);
-
-    destroy_graph(ne);
+    //bst_destroy(Root);
 
     return 0;
 }
---
+++ tree.h
@@ -0,0 +1,23 @@
+#ifndef __TREE_H__
+#define __TREE_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <assert.h>
+
+typedef struct Tree_node Tree_node;
+
+Tree_node* __bst_create_node(int New_value);
+
+void __bst_insert(Tree_node** Root, Tree_node* New_node);
+
+void bst_insert(Tree_node** Root, int New_value);
+
+void __bst_display(Tree_node* Root);
+
+void bst_display(Tree_node* Root);
+
+void bst_destroy(Tree_node* Root);
+
+#endif
