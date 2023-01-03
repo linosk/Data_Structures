c9f3c26ba9672d14933dfbed01f3d6663b9095d3
linosk <linakoskar@gmail.com>
2023-01-03 10:02:30 +0100

    Inserting, displaying and destroying bst implemented correctly.

--- main.c
+++ main.c
@@ -4,16 +4,18 @@ int main(int argc, char** argv)
 {
     Tree_node* Root = NULL;
     bst_insert(&Root,4);
-    bst_insert(&Root,3);
-    bst_insert(&Root,5);
-    bst_insert(&Root,1);
     bst_insert(&Root,2);
     bst_insert(&Root,6);
     bst_insert(&Root,1);
+    bst_insert(&Root,3);
+    bst_insert(&Root,5);
+    bst_insert(&Root,7);
 
     bst_display(Root);
 
-    //bst_destroy(Root);
+    bst_destroy(Root);
+    
+    //bst_find(Root,2);
 
     return 0;
 }
